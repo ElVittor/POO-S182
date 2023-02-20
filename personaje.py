1b08f645e8e8945768ebc422e0588f2e94f4d124
ElVittor <93359780+ElVittor@users.noreply.github.com>
2023-02-20 15:28:49 -0600

    creamos método correr
    ahora sabes si corre o no, estado es boolean

--- personaje.py
+++ personaje.py
@@ -1,5 +1,15 @@
 class Personaje:
+
 #atributos
+
 especie = "Human"
 nombre = "MasterChief"
 altura = 2.7
+
+#Metodos
+
+def correr(self,estado): #al usar el self le digo que es un metodo no una funcion, agregamos una coma para el parametro a usar
+    if(estado):
+        print("El Personaje "+self.nombre+" está corriendo")
+    else:
+        print("El personaje "+self.nombre+" se detuvo.")
