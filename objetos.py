d146176ab916cb3f6fc1d00e37af9435adfeb287
ElVittor <93359780+ElVittor@users.noreply.github.com>
2023-02-20 15:46:42 -0600

    creamos la clase objetos
    desde objetos llamamamos la clase personaje para hacer uso de sus atributos

---
+++ objetos.py
@@ -0,0 +1,9 @@
+from personaje import *
+
+# creamos la instancia de la clase personaje ahora llamada (la instancia) como Heroe
+Heroe = Personaje()
+
+# Usamos los atributos 
+print("El personaje se llama: "+Heroe.nombre)
+print("El personaje pertenenece a la especie : "+Heroe.especie)
+print("El personaje tiene una altura de : "+str(Heroe.altura))
--- personaje.py
+++ personaje.py
@@ -2,14 +2,23 @@ class Personaje:
 
 #atributos
 
-especie = "Human"
-nombre = "MasterChief"
-altura = 2.7
+    especie = "Human"
+    nombre = "MasterChief"
+    altura = 2.7
 
-#Metodos
+    #Metodos
 
-def correr(self,estado): #al usar el self le digo que es un metodo no una funcion, agregamos una coma para el parametro a usar
-    if(estado):
-        print("El Personaje "+self.nombre+" está corriendo")
-    else:
-        print("El personaje "+self.nombre+" se detuvo.")
+    def correr(self,estado): #al usar el self le digo que es un metodo no una funcion, agregamos una coma para el parametro a usar
+        if(estado):
+            print("El Personaje "+self.nombre+" está corriendo")
+        else:
+            print("El personaje "+self.nombre+" se detuvo.")
+            
+    def lanzargranadas(self):
+        print("Granada lanzada por "+self.nombre+" youre dead")
+        
+    def recargararma(self,municion):
+        cargador = 10
+        cargador=cargador + municion
+        print("Rifle de asalto de "+self.nombre+" ha sido recargado y tiene "+cargador+" municiones")
+ 
