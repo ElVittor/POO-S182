29cb61d62dfd34f55d10afe72707ba5c30c03ca4
ElVittor <93359780+ElVittor@users.noreply.github.com>
2023-04-19 15:00:38 -0600

    Comit 2.50
    se creo el controladorm falta la interfaz gráfica

---
+++ examen3p/ControladoresBD.py
@@ -0,0 +1,62 @@
+from tkinter import messagebox
+import sqlite3
+
+
+class controladorBD:
+    def __init__(self):
+        pass
+    
+    def conexionBD(self):
+        try:
+            conexion=sqlite3.connect("C:/Users/leon_/Documents/UPQ/5° Cuatri/PROGRMACIÓN OO/POO-S182/examen3p/BDImportaciones.db")
+            print("Conectado a BD")
+            return conexion
+        except sqlite3.OperationalError:
+            print("No se pudo Conectar")
+            
+    def guardar(self,mer,pai):
+        conx=self.conexionBD()
+        if(mer=="" or pai==""):
+            messagebox.showwarning("Cuidado","Formulario incompleto")
+            conx.close()
+        else:
+            cursor=conx.cursor()
+            sqlinsert="insert into TB_Europa(Mercancia,Pais) values(?,?)"
+            cursor.execute(sqlinsert,mer,pai)
+            conx.commit()
+            conx.close
+            messagebox.showinfo("Exito","Usuario guardado")
+            
+    def consultar(self,id):
+        conx=self.conexionBD() #Para acceder a la funcion conexion
+        if(id==""):
+                messagebox.showwarning("Cuidado","Escribe un Identificdor")
+                conx.close()
+        else:
+            try:
+                cursor=conx.cursor()
+                sqlSelect="Select* from TB_Europa where IDImpo = "+id
+                cursor.execute(sqlSelect)
+                RSusuario=cursor.fetchall()#result set o variable para guardar la información de la consulta, lo que tenga cursor lo pasamos a la variabale para seso sirve la funcion cursor.fetchall()
+                conx.close()
+                return RSusuario
+            except sqlite3.OperationalError:
+                print("Error de Consulta")
+        
+    def eliminar(self,id):
+        pass
+        conx=self.conexionBD()
+        cursor=conx.cursor()
+        sqldelete="DELETE FROM TB_Europa WHERE IDImpo="+id
+        
+        if(id==""):
+                messagebox.showwarning("Cuidado","Escribe un Identificdor")
+                conx.close()
+        else:
+            try:
+                cursor.execute(sqldelete)
+                conx.commit()
+                conx.close()
+                messagebox.showinfo("Correcto","Entrada Eliminada")
+            except sqlite3.OperationalError:
+                print("Error de Consulta")
---
+++ examen3p/mainexamen.py
@@ -0,0 +1,5 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+from ControladoresBD import *
+controlador=controladorBD()
